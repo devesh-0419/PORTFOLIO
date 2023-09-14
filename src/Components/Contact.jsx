1f6273eecc09865ff63a2ac4ac5e43bb8dd3365c
Devesh Swarnkar <deveshswarnkar007@gmail.com>
2023-09-14 18:17:29 +0530

    Added message backend not connected

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -19,7 +19,7 @@ const handleChange = e =>{
   e.preventDefault();
  try {
    console.log('Submitted');
-  
+     
  } catch (e) {
   console.error(e)
  }
@@ -30,6 +30,7 @@ const handleChange = e =>{
         <div className='flex flex-col items-center justify-center'>
         <div className='flex flex-col items-center justify-center mt-9'>
     <Title title={'Contact'} text={'Feel free to Contact me by submitting the form below and I will get back to you as soon as possible'}/>
+    
         </div>
 
         </div>
@@ -43,11 +44,11 @@ const handleChange = e =>{
             <textarea onChange={handleChange} name='email' value={contact.email} type="text" id='email' className='bg-purple-400 mx-4 rounded-md p-1 pl-3 placeholder:text-black/30 py-2 text-xs overflow-y-hidden break-words max-w-full' placeholder='Enter Your Email' autoComplete="off" />
             <label htmlFor="message" className=' mx-4 my-2 text-sm font-semibold uppercase'>message</label>
             <textarea onChange={handleChange} name='message' value={contact.message} type="text" id='message' className='bg-purple-400 mx-4 rounded-md p-1 pl-3 placeholder:text-black/30 py-2 text-xs overflow-y-hidden break-words max-w-full h-28' placeholder='Enter Your Message...' autoComplete="off"/>
-
+            
             <button type='submit' className="rounded-md mx-4 my-6 p-2 text-sm font-semibold bg-purple-600 shadow shadow-slate-800 text-slate-100 active:scale-110 ">
               <h1 className="">Submit</h1>
             </button>
-            
+            <p className='text-xs text-red-500'>*Not connected with backend</p>
         </form>
     </div>
    </div>
